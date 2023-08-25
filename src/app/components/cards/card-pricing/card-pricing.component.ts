b611e42d14c29aa3b1bb94e9f3b211a7e03d2ae3
Zwiicker <hickzwicker@gmail.com>
2023-08-24 22:33:36 -0300

    Criando Card-Pricing

--- src/app/components/cards/card-pricing/card-pricing.component.ts
+++ src/app/components/cards/card-pricing/card-pricing.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card-pricing',
-  templateUrl: './card-pricing.component.html',
-  styleUrls: ['./card-pricing.component.css']
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
 })
-export class CardPricingComponent {
+export class CardComponent implements OnInit {
+
+	@Input()
+	gameCover:string = ""
+	@Input()
+	gameLabel:string=""
+	@Input()
+	gameType:string ="XPTO | PS4"
+	@Input()
+	gamePrice:string = "R$ 399,90"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
