43e6371d0edc6af7d1a464036ed67a964032d4ca
Zwiicker <hickzwicker@gmail.com>
2023-08-24 21:54:36 -0300

    Criado a base dos components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardsComponent,
+    MenuBarComponent,
+
   ],
   imports: [
     BrowserModule,
